26a3902aa9b0fcee5582b555feec84ff85a06a11
Michał Wiśniewski <michu123qwe@gmail.com>
2019-11-19 19:50:46 +0100

    register models to admin site

--- StudentPlatform/platformapp/admin.py
+++ StudentPlatform/platformapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Group, Tab, Element, UserGroupRelation, Comment
 
 # Register your models here.
+admin.site.register(Group)
+admin.site.register(Tab)
+admin.site.register(Element)
+admin.site.register(UserGroupRelation)
+admin.site.register(Comment)
