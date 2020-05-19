3a8fe72c48f25e50e4bf2948e9cd8856c678b06f
questras <michu123qwe@gmail.com>
2020-05-19 14:39:50 +0200

    add element view with tests

---
+++ StudentPlatform/platformapp/tests/tests_element.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+from . import utils_for_testing as utils
+from .. import scripts
+from ..models import Group, Tab, Element
+
+
+class ElementViewTests(TestCase):
+    """Tests for element_view."""
+
+    def setUp(self) -> None:
+        self.not_logged_user = utils.create_user('notlogged', 'notlogged')
+        self.group = scripts.create_group('test', 'test', self.not_logged_user)
+        self.tab = scripts.create_tab('test', self.not_logged_user, self.group)
+        self.element = scripts.create_element('test', 'test',
+                                              self.not_logged_user, self.tab)
+        self.url = reverse('element_view',
+                           args=(self.group.pk, self.tab.pk, self.element.pk,))
+
+    def test_not_logged_cannot_access(self):
+        """Test if not logged user cannot access view."""
+
+        utils.test_not_logged_cannot_access(self, self.url)
+
+    def test_logged_user_not_in_group_cannot_access(self):
+        """Test if logged user not in element's group cannot
+        access view."""
+
+        utils.create_user_and_authenticate(self)
+        expected_url = reverse('my_groups_view')
+
+        response = self.client.get(self.url)
+        self.assertRedirects(response, expected_url=expected_url)
+
+    def test_logged_user_in_group_can_access(self):
+        """Test if logged user in element's group can access view."""
+
+        self.client.login(username='notlogged', password='notlogged')
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
--- StudentPlatform/platformapp/views/element_views.py
+++ StudentPlatform/platformapp/views/element_views.py
@@ -1,4 +1,8 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+
+from ..models import Group, Tab, Element
 
 
 class CreateElementView(CreateView):
@@ -13,5 +17,17 @@ class DeleteElementView(DeleteView):
     pass
 
 
+@login_required
 def element_view(request, g_pk, t_pk, pk):
-    pass
+    group = get_object_or_404(Group, pk=g_pk)
+    tab = get_object_or_404(Tab, pk=t_pk)
+    element = get_object_or_404(Element, pk=pk)
+
+    if request.user not in group.users.all():
+        return redirect(reverse('my_groups_view'))
+
+    context = {
+        'element': element,
+    }
+
+    return render(request, 'platformapp/element_view.html', context)
