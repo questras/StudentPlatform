1928ada5e32fe4a48d3e6c8991dc2aa4146e57c0
questras <michu123qwe@gmail.com>
2020-07-08 17:01:34 +0200

    change usage of logged_user in tests to be more explicit

--- StudentPlatform/platformapp/tests/utils_for_testing.py
+++ StudentPlatform/platformapp/tests/utils_for_testing.py
@@ -1,35 +1,39 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.shortcuts import reverse
-from typing import List
+from typing import List, Tuple
 
 User = get_user_model()
 LOGIN_URL = reverse('login_view')
 
 
-def create_user(name, password):
+def create_user(name: str, password: str) -> User:
     """Create user for testing purposes."""
 
     user = User.objects.create_user(username=name, password=password)
     return user
 
 
-def create_user_and_authenticate(test_case: TestCase):
-    """Create user for testing purposes, save him as
-    test_case's variable [logged_user] and log in."""
+def create_user_and_authenticate(test_case: TestCase) -> User:
+    """Create user for testing purposes, log in and return
+    authenticated user."""
 
-    test_case.logged_user = create_user('logged', 'logged')
+    logged_user = create_user('logged', 'logged')
     test_case.client.login(username='logged', password='logged')
 
+    return logged_user
 
-def create_two_users_authenticate_one(test_case: TestCase):
-    """Create two users for testing purposes, save them
-    as test_case's variables [logged_user] and [not_logged_user]
-    and authenticate [logged_user]."""
 
-    test_case.logged_user = create_user('logged', 'logged')
-    test_case.not_logged_user = create_user('notlogged', 'notlogged')
+def create_two_users_authenticate_one(test_case: TestCase) \
+        -> Tuple[User]:
+    """Create two users for testing purposes, authenticate
+    one and return them."""
+
+    logged_user = create_user('logged', 'logged')
+    not_logged_user = create_user('notlogged', 'notlogged')
     test_case.client.login(username='logged', password='logged')
+
+    return logged_user, not_logged_user
 
 
 def login_redirect_url(url):
